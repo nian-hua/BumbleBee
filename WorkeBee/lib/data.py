592437032410372a8a49bbf408c566e4b112a9c0
nian-hua <41846038+nian-hua@users.noreply.github.com>
2019-12-29 16:24:06 +0800

    Create data.py

---
+++ WorkeBee/lib/data.py
@@ -0,0 +1,17 @@
+import os
+
+PATHS_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../")
+PATHS_POCS = os.path.join(PATHS_ROOT, "pocs")
+PATHS_OUTPUT = os.path.join(PATHS_ROOT, "output")
+VERSION = "v0.001"
+POCS =[]
+
+
+QUEUEHOST  = "192.168.93.174"
+QUEUEPORT  = 18888
+TASKPATH   = "/task"
+SCRIPT = None
+TOKEN = "243e21045c0c2912f5315907c8fc0775"
+TASK = None
+BUMBLEID = ""
+RESULT = None
